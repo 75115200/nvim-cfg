ff4907f0f3c9c4ff18fc0516ed0f049f8b3afd23
zhenshuo.zeng <h75115200@gmail.com>
2022-07-02 18:20:55 +0800

    add mappings&opts

--- mappings.lua
+++ mappings.lua
@@ -1,3 +1,31 @@
 local M = {}
 
+
+M.disabled = {
+  n = {
+      ["<C-c>"] = "",
+  }
+}
+
+M.general = {
+  n = {
+    ["<C-e>"] = { "<cmd> Telescope oldfiles <CR>", "   find oldfiles" },
+    ["<C-r>"] = {
+      function()
+        require("nvchad.ui.renamer").open()
+      end,
+      "   lsp rename",
+    },
+  },
+
+  i = {
+      ["<C-r>"] = {
+         function()
+            require("nvchad.ui.renamer").open()
+         end,
+         "   lsp rename",
+      },
+  }
+}
+
 return M
